fbdfee1e62c18142bd3a5b3aa6b3f5a28998549a
Robin Goyal <goyal.rob@gmail.com>
2017-11-08 22:24:47 -0500

    Added comments to file

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Name: main.py
+# Author: Robin Goyal
+# Last-Modified: November 8, 2017
+# Purpose: Retrieve basic information regarding currently saved tracks
+#          on my profile for future use
+
 import requests
 import configparser
 import xlsxwriter
@@ -12,10 +18,15 @@ def get_auth_token():
     return AUTH_TOKEN
 
 def create_csv():
+
+    # Initialize workbook and worksheet
     workbook = xlsxwriter.Workbook('spotifysongs.xlsx')
     worksheet = workbook.add_worksheet()
 
+    # Create formatter
     bold = workbook.add_format({'bold': True})
+
+    # Declare headings for worksheet
     worksheet.write('A1', 'Track ID', bold)
     worksheet.write('B1', 'Track Name', bold)
     worksheet.write('C1', 'Album Name', bold)
@@ -25,28 +36,38 @@ def create_csv():
 
 
 def close_csv(workbook):
+    '''
+    workbook: workbook file created for song data
+    '''
 
     workbook.close()
 
 def main():
 
+    # Retrieve authentication token
     auth_token = get_auth_token()
 
+    # Get worksheet and workbook pointer
     worksheet, workbook = create_csv()
 
+    # Setup data regarding initial request
     header = {'Authorization': 'Bearer {}'.format(auth_token)}
     payload = {'limit': 50}
     url = "https://api.spotify.com/v1/me/tracks"
 
-
+    # Initialize row to 1 to account for header row
     row = 1
 
     while True:
+
+        # Perform initial request
         r = requests.get(url, headers = header, params = payload)
+
         # Check if status code returned 200
         if r.status_code == 200:
             r = r.json()
 
+            # Append track information to sheet
             for item in r['items']:
                 worksheet.write(row, 0, item['track']['id'])
                 worksheet.write(row, 1, item['track']['name'])
@@ -54,14 +75,19 @@ def main():
 
                 row += 1
 
-            # Check if any songs are remaining
+            # Check if there are no songs remaining
             if r['next'] == None:
                 break
+
+            # Retrieve url for next request
             else:
                 url = r['next']
+
+        # Print error message for debugging
         else:
             print(r.text)
 
+    # Close csv file
     close_csv(workbook)
 
 if __name__=="__main__":
